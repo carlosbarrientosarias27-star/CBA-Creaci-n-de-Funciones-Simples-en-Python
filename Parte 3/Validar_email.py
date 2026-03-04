30fc44b17649704a10f1777a423982cf42c1d3a6
carlosbarrientosarias27-star <carlos.barrientosarias27@gmail.com>
2026-03-04 09:56:51 +0100

    parte 3

--- Parte 3/Validar_email.py
+++ Parte 3/Validar_email.py
@@ -3,3 +3,7 @@ def validar_email(email):
         return True
     else:
         return False
+    
+# Ejemplos de uso 
+print(validar_email("usuario@ejemplo.com")) # True
+print(validar_email("usuario.com")) # False
